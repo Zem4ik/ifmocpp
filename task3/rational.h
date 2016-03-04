9ab8389b70f42771b5280aa40e2e98c777fa6a4f
Zem4ik <vladbullzv@gmail.com>
2016-03-04 23:03:26 +0300

    Create rational.h

---
+++ task3/rational.h
@@ -0,0 +1,27 @@
+class rational {
+    private:
+        int num;
+        int denom;
+
+        int gcd(int a, int b) const;
+
+    public:
+        rational(
+        int a);
+
+        rational(
+        int a,
+        int b);
+
+        int getNum() const;
+
+        int getDenom() const;
+
+        rational operator +(rational const &secondRational) const;
+
+        rational operator -(rational const &secondRational) const;
+
+        rational operator *(rational const &secondRational) const;
+
+        rational operator /(rational const &secondRational) const;
+};

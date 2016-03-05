f9d11cb64c0b24ba66a606b3777fd64d338979f5
Zem4ik <vladbullzv@gmail.com>
2016-03-05 12:41:53 +0300

    Update rational.h

--- task3/rational.h
+++ task3/rational.h
@@ -4,26 +4,15 @@ class rational {
     private:
         int num;
         int denom;
-
         int gcd(int a, int b) const;
 
     public:
-        rational(
-        int a);
-
-        rational(
-        int a,
-        int b);
-
+        rational(int a);
+        rational(int a, int b);
         int getNum() const;
-
         int getDenom() const;
-
         rational operator +(rational const &secondRational) const;
-
         rational operator -(rational const &secondRational) const;
-
         rational operator *(rational const &secondRational) const;
-
         rational operator /(rational const &secondRational) const;
 };

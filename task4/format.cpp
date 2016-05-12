a29467463f224c9ea22a72762b4a293660747622
Zem4ik <vladbullzv@gmail.com>
2016-05-12 23:38:42 +0300

    Add files via upload

--- task4/format.cpp
+++ task4/format.cpp
@@ -285,7 +285,7 @@ namespace Format {
                 answer += "%";
                 indexofFormatString += 2;
             }
-            if (format[indexofFormatString] == '%' && getSpecifier(format) != none) {
+            if (format[indexofFormatString++] == '%' && getSpecifier(format) != none) {
                 throw std::out_of_range("not enough arguments");
             }
             if (format[indexofFormatString] == '%' && getSpecifier(format) == none) {
